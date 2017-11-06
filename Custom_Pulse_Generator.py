32e306b0d99d56d70ab40344a4334f7793017a2e
Narek Shukhyan <narekshukian@gmail.com>
2017-11-07 00:53:48 +0400

    Initial versions

--- Custom_Pulse_Generator.py
+++ Custom_Pulse_Generator.py
@@ -1,1 +1,10 @@
+#input math
+verilog = open ("C:\\Users\\nshukhy\\Documents\\GitHub\\Custom_Pulse_Generator\\verilog.txt", rw)
+f = open ("C:\\Users\\nshukhy\\Documents\\GitHub\\Custom_Pulse_Generator\\input.txt", rw);
+
+verilog.write ("module wave_generator(" + \n + "clk," + \n + "reset," + \n + "wave_1" + \n + ");" + \n + \n);
+
+
+
+
 
